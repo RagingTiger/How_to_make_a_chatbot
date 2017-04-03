bae19f3acf91496aaa7345745bd47e0403b25b2b
John Anderson <jander43@vols.utk.edu>
2017-04-03 08:41:45 -0400

    Refactored memorynetwork.py code into a class

--- memorynetwork.py
+++ memorynetwork.py
@@ -9,11 +9,14 @@ References:
 Reaches 98.6% accuracy on task 'single_supporting_fact_10k' after 120 epochs.
 Time per epoch: 3s on CPU (core i7).
 '''
-from __future__ import print_function
 
+
+# libs
+from __future__ import print_function
 from keras.models import Sequential, Model
 from keras.layers.embeddings import Embedding
-from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate
+from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot,
+concatenate
 from keras.layers import LSTM
 from keras.utils.data_utils import get_file
 from keras.preprocessing.sequence import pad_sequences
@@ -21,8 +24,10 @@ from functools import reduce
 import tarfile
 import numpy as np
 import re
+import os
 
 
+# funcs
 def tokenize(sent):
     '''Return the tokens of a sentence including punctuation.
     >>> tokenize('Bob dropped the apple. Where is the apple?')
@@ -92,138 +97,161 @@ def vectorize_stories(data, word_idx, story_maxlen, query_maxlen):
     return (pad_sequences(X, maxlen=story_maxlen),
             pad_sequences(Xq, maxlen=query_maxlen), np.array(Y))
 
-try:
-    path = get_file('babi-tasks-v1-2.tar.gz', origin='https://s3.amazonaws.com/text-datasets/babi_tasks_1-20_v1-2.tar.gz')
-except:
-    print('Error downloading dataset, please download it manually:\n'
-          '$ wget http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz\n'
-          '$ mv tasks_1-20_v1-2.tar.gz ~/.keras/datasets/babi-tasks-v1-2.tar.gz')
-    raise
-tar = tarfile.open(path)
 
-challenges = {
-    # QA1 with 10,000 samples
-    'single_supporting_fact_10k': 'tasks_1-20_v1-2/en-10k/qa1_single-supporting-fact_{}.txt',
-    # QA2 with 10,000 samples
-    'two_supporting_facts_10k': 'tasks_1-20_v1-2/en-10k/qa2_two-supporting-facts_{}.txt',
-}
-challenge_type = 'single_supporting_fact_10k'
-challenge = challenges[challenge_type]
+class MemNet(object):
+    """Wrapper for memorynetwork code."""
+    def __init__(self, save=False):
+        # store save flag
+        self.save
 
-print('Extracting stories for the challenge:', challenge_type)
-train_stories = get_stories(tar.extractfile(challenge.format('train')))
-test_stories = get_stories(tar.extractfile(challenge.format('test')))
+        # check if model exists
+        if os.path.exists('chatbot.h5'):
+            # TODO: write code to load model
+            pass
+        else:
+            # prompt user to build model?
+            answer = raw_input('No model found. Build model? [Y/n]: ')
+            if answer == 'Y':
+                self.build_model()
 
-vocab = set()
-for story, q, answer in train_stories + test_stories:
-    vocab |= set(story + q + [answer])
-vocab = sorted(vocab)
+    def build_model(self):
+        """Simply a wrapper to build memory network model for above example."""
+        try:
+            path = get_file('babi-tasks-v1-2.tar.gz', origin='https://s3.amazonaws.com/text-datasets/babi_tasks_1-20_v1-2.tar.gz')
+        except:
+            print('Error downloading dataset, please download it manually:\n'
+                  '$ wget http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz\n'
+                  '$ mv tasks_1-20_v1-2.tar.gz ~/.keras/datasets/babi-tasks-v1-2.tar.gz')
+            raise
+        tar = tarfile.open(path)
 
-# Reserve 0 for masking via pad_sequences
-vocab_size = len(vocab) + 1
-story_maxlen = max(map(len, (x for x, _, _ in train_stories + test_stories)))
-query_maxlen = max(map(len, (x for _, x, _ in train_stories + test_stories)))
+        challenges = {
+            # QA1 with 10,000 samples
+            'single_supporting_fact_10k': 'tasks_1-20_v1-2/en-10k/qa1_single-supporting-fact_{}.txt',
+            # QA2 with 10,000 samples
+            'two_supporting_facts_10k': 'tasks_1-20_v1-2/en-10k/qa2_two-supporting-facts_{}.txt',
+        }
+        challenge_type = 'single_supporting_fact_10k'
+        challenge = challenges[challenge_type]
 
-print('-')
-print('Vocab size:', vocab_size, 'unique words')
-print('Story max length:', story_maxlen, 'words')
-print('Query max length:', query_maxlen, 'words')
-print('Number of training stories:', len(train_stories))
-print('Number of test stories:', len(test_stories))
-print('-')
-print('Here\'s what a "story" tuple looks like (input, query, answer):')
-print(train_stories[0])
-print('-')
-print('Vectorizing the word sequences...')
+        print('Extracting stories for the challenge:', challenge_type)
+        train_stories = get_stories(tar.extractfile(challenge.format('train')))
+        test_stories = get_stories(tar.extractfile(challenge.format('test')))
 
-word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
-inputs_train, queries_train, answers_train = vectorize_stories(train_stories,
-                                                               word_idx,
-                                                               story_maxlen,
-                                                               query_maxlen)
-inputs_test, queries_test, answers_test = vectorize_stories(test_stories,
-                                                            word_idx,
-                                                            story_maxlen,
-                                                            query_maxlen)
+        vocab = set()
+        for story, q, answer in train_stories + test_stories:
+            vocab |= set(story + q + [answer])
+        vocab = sorted(vocab)
 
-print('-')
-print('inputs: integer tensor of shape (samples, max_length)')
-print('inputs_train shape:', inputs_train.shape)
-print('inputs_test shape:', inputs_test.shape)
-print('-')
-print('queries: integer tensor of shape (samples, max_length)')
-print('queries_train shape:', queries_train.shape)
-print('queries_test shape:', queries_test.shape)
-print('-')
-print('answers: binary (1 or 0) tensor of shape (samples, vocab_size)')
-print('answers_train shape:', answers_train.shape)
-print('answers_test shape:', answers_test.shape)
-print('-')
-print('Compiling...')
+        # Reserve 0 for masking via pad_sequences
+        vocab_size = len(vocab) + 1
+        story_maxlen = max(map(len, (x for x, _, _ in train_stories + test_stories)))
+        query_maxlen = max(map(len, (x for _, x, _ in train_stories + test_stories)))
 
-# placeholders
-input_sequence = Input((story_maxlen,))
-question = Input((query_maxlen,))
+        print('-')
+        print('Vocab size:', vocab_size, 'unique words')
+        print('Story max length:', story_maxlen, 'words')
+        print('Query max length:', query_maxlen, 'words')
+        print('Number of training stories:', len(train_stories))
+        print('Number of test stories:', len(test_stories))
+        print('-')
+        print('Here\'s what a "story" tuple looks like (input, query, answer):')
+        print(train_stories[0])
+        print('-')
+        print('Vectorizing the word sequences...')
 
-# encoders
-# embed the input sequence into a sequence of vectors
-input_encoder_m = Sequential()
-input_encoder_m.add(Embedding(input_dim=vocab_size,
-                              output_dim=64))
-input_encoder_m.add(Dropout(0.3))
-# output: (samples, story_maxlen, embedding_dim)
+        word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+        inputs_train, queries_train, answers_train = vectorize_stories(train_stories,
+                                                                       word_idx,
+                                                                       story_maxlen,
+                                                                       query_maxlen)
+        inputs_test, queries_test, answers_test = vectorize_stories(test_stories,
+                                                                    word_idx,
+                                                                    story_maxlen,
+                                                                    query_maxlen)
 
-# embed the input into a sequence of vectors of size query_maxlen
-input_encoder_c = Sequential()
-input_encoder_c.add(Embedding(input_dim=vocab_size,
-                              output_dim=query_maxlen))
-input_encoder_c.add(Dropout(0.3))
-# output: (samples, story_maxlen, query_maxlen)
+        print('-')
+        print('inputs: integer tensor of shape (samples, max_length)')
+        print('inputs_train shape:', inputs_train.shape)
+        print('inputs_test shape:', inputs_test.shape)
+        print('-')
+        print('queries: integer tensor of shape (samples, max_length)')
+        print('queries_train shape:', queries_train.shape)
+        print('queries_test shape:', queries_test.shape)
+        print('-')
+        print('answers: binary (1 or 0) tensor of shape (samples, vocab_size)')
+        print('answers_train shape:', answers_train.shape)
+        print('answers_test shape:', answers_test.shape)
+        print('-')
+        print('Compiling...')
 
-# embed the question into a sequence of vectors
-question_encoder = Sequential()
-question_encoder.add(Embedding(input_dim=vocab_size,
-                               output_dim=64,
-                               input_length=query_maxlen))
-question_encoder.add(Dropout(0.3))
-# output: (samples, query_maxlen, embedding_dim)
+        # placeholders
+        input_sequence = Input((story_maxlen,))
+        question = Input((query_maxlen,))
 
-# encode input sequence and questions (which are indices)
-# to sequences of dense vectors
-input_encoded_m = input_encoder_m(input_sequence)
-input_encoded_c = input_encoder_c(input_sequence)
-question_encoded = question_encoder(question)
+        # encoders
+        # embed the input sequence into a sequence of vectors
+        input_encoder_m = Sequential()
+        input_encoder_m.add(Embedding(input_dim=vocab_size,
+                                      output_dim=64))
+        input_encoder_m.add(Dropout(0.3))
+        # output: (samples, story_maxlen, embedding_dim)
 
-# compute a 'match' between the first input vector sequence
-# and the question vector sequence
-# shape: `(samples, story_maxlen, query_maxlen)`
-match = dot([input_encoded_m, question_encoded], axes=(2, 2))
-match = Activation('softmax')(match)
+        # embed the input into a sequence of vectors of size query_maxlen
+        input_encoder_c = Sequential()
+        input_encoder_c.add(Embedding(input_dim=vocab_size,
+                                      output_dim=query_maxlen))
+        input_encoder_c.add(Dropout(0.3))
+        # output: (samples, story_maxlen, query_maxlen)
 
-# add the match matrix with the second input vector sequence
-response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
-response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
+        # embed the question into a sequence of vectors
+        question_encoder = Sequential()
+        question_encoder.add(Embedding(input_dim=vocab_size,
+                                       output_dim=64,
+                                       input_length=query_maxlen))
+        question_encoder.add(Dropout(0.3))
+        # output: (samples, query_maxlen, embedding_dim)
 
-# concatenate the match matrix with the question vector sequence
-answer = concatenate([response, question_encoded])
+        # encode input sequence and questions (which are indices)
+        # to sequences of dense vectors
+        input_encoded_m = input_encoder_m(input_sequence)
+        input_encoded_c = input_encoder_c(input_sequence)
+        question_encoded = question_encoder(question)
 
-# the original paper uses a matrix multiplication for this reduction step.
-# we choose to use a RNN instead.
-answer = LSTM(32)(answer)  # (samples, 32)
+        # compute a 'match' between the first input vector sequence
+        # and the question vector sequence
+        # shape: `(samples, story_maxlen, query_maxlen)`
+        match = dot([input_encoded_m, question_encoded], axes=(2, 2))
+        match = Activation('softmax')(match)
 
-# one regularization layer -- more would probably be needed.
-answer = Dropout(0.3)(answer)
-answer = Dense(vocab_size)(answer)  # (samples, vocab_size)
-# we output a probability distribution over the vocabulary
-answer = Activation('softmax')(answer)
+        # add the match matrix with the second input vector sequence
+        response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
+        response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
 
-# build the final model
-model = Model([input_sequence, question], answer)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
-              metrics=['accuracy'])
+        # concatenate the match matrix with the question vector sequence
+        answer = concatenate([response, question_encoded])
 
-# train
-model.fit([inputs_train, queries_train], answers_train,
-          batch_size=32,
-          epochs=120,
-          validation_data=([inputs_test, queries_test], answers_test))
+        # the original paper uses a matrix multiplication for this reduction step.
+        # we choose to use a RNN instead.
+        answer = LSTM(32)(answer)  # (samples, 32)
+
+        # one regularization layer -- more would probably be needed.
+        answer = Dropout(0.3)(answer)
+        answer = Dense(vocab_size)(answer)  # (samples, vocab_size)
+        # we output a probability distribution over the vocabulary
+        answer = Activation('softmax')(answer)
+
+        # build the final model
+        model = Model([input_sequence, question], answer)
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        # train
+        model.fit([inputs_train, queries_train], answers_train,
+                  batch_size=32,
+                  epochs=120,
+                  validation_data=([inputs_test, queries_test], answers_test))
+
+        # save
+        if self.save:
+            model.save('chatbot.h5')
