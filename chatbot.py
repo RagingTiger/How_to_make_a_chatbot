bb02285308b60d07786158a7fbe33d1a36e4b0e2
John Anderson <jander43@vols.utk.edu>
2017-04-04 08:56:45 -0400

    Started implementation of interactive prompt for interfacing with chatbot

--- chatbot.py
+++ chatbot.py
@@ -10,21 +10,50 @@ Usage:
     chatbot help
 """
 
+# libs
+import getpass
+import memorynetwork as memnet
+
+# globals
+USERNAME = getpass.getuser()
+MACHINE = 'chatbot'
+PROMPT = memnet.colortxt('{0}@{1}: '.format(USERNAME, MACHINE))
+HALTING = memnet.colortxt('\nShutting down chatbot ...', 'red')
+
 
 # classes
 class ChatBot(object):
     """Interface to be used with fire module."""
-    def qa(self, save=None):
-        # get mem net
-        import memorynetwork as memnet
+    def __init__(self):
+        # cache model
+        self.model = None
 
+    def qa(self, save=None):
         # check save val
         save = True if save is not None else False
         if save:
             print memnet.colortxt('Model will be saved', 'red')
 
         # build
-        model = memnet.MemNet(save)
+        self.model = memnet.MemNet(save)
+
+        # run cli
+        self._chatbot_cli()
+
+    def _chatbot_cli(self):
+        """Run interactive prompt."""
+        # start loop
+        while 1:
+            # get question
+            try:
+                question = raw_input(PROMPT)
+
+            except EOFError:
+                memnet.sys.exit(HALTING)
+
+            except KeyboardInterrupt:
+                print '\n'
+                continue
 
 
 # executable
